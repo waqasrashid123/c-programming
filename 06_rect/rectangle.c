18708999e27588c6b77a1a41ce9f4ffeb585458c
Coursera Learner <nobody@nowhere.nul>
2020-09-27 14:40:20 +0000

    Task 06

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 //I've provided "min" and "max" functions in
 //case they are useful to you
+
 int min (int a, int b) {
   if (a < b) {
     return a;
@@ -16,15 +17,47 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rect{ int x; int y; int width; int height;};
+typedef struct rect rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width<0)
+    {
+      r.x = r.x +r.width;
+      r.width = 0 - r.width;
+    }
+  if (r.height < 0)
+	{
+	  r.y= r.y + r.height;
+	  r.height = 0 - r.height;
+	}
   return r;
 }
+
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+//WRITE THIS FUNCTION
+
+  rectangle r3;
+  rectangle r4;
+  r4.width = 0;
+  r4.height = 0;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  r3.x = max(r1.x, r2.x);
+  r3.width = min(r1.x+r1.width,r2.x+r2.width)-r3.x;
+  r3.y = max(r1.y, r2.y);
+  r3.height = min(r1.y+r1.height,r2.y+r2.height)-r3.y;
+  r3 = canonicalize(r3);
+
+  if(((r1.x > r2.x+r2.width)||(r2.x > r1.x+r1.width)) ||
+     ((r1.y > r2.y+r2.height)|| (r2.y>r1.y+r1.height))){
+    return r4;
+  }
+
+  return r3;
+
+  
 }
 
 //You should not need to modify any code below this line
